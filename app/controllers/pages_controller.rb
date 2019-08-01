2feba2406981fdd3593dac0b52bea8c1620fd320
Claire Gautier <claire.gautier@edhec.com>
2019-08-01 18:51:45 +0200

    prepared basic layout navbar banner and next container for posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @blogposts = Blogpost.order('created_at DESC').first(3)
   end
 end
