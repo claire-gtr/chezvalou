d24b9c3ad0c0bb16da5382448742e55ae5ba6845
Claire Gautier <claire.gautier@edhec.com>
2019-08-12 11:04:27 +0200

    Added meta tag helper

---
+++ config/sitemap.rb
@@ -0,0 +1,10 @@
+# SitemapGenerator::Sitemap.default_host = 'https://www.chezvalou-patisserie.com'
+# SitemapGenerator::Sitemap.create do
+#   add root_path, :changefreq => 'daily', :priority => 1
+#   #
+#   Blogpost.find_each do |post|
+#     add blogpost_path(post), :priority => 0.9, :lastmod => post.updated_at, :changefreq => 'daily'
+#   end
+
+# end
+# SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
