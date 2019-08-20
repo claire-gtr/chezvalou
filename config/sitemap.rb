0f299ca27d3e2974ac5f8454e33e218a34093c60
Claire Gautier <claire.gautier@edhec.com>
2019-08-20 10:27:09 +0200

    Set up sitempa and background banner

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,10 +1,10 @@
-# SitemapGenerator::Sitemap.default_host = 'https://www.chezvaloupatisserie.fr'
-# SitemapGenerator::Sitemap.create do
-#   add root_path, :changefreq => 'daily', :priority => 1
-#   #
-#   Blogpost.find_each do |post|
-#     add blogpost_path(post), :priority => 0.9, :lastmod => post.updated_at, :changefreq => 'daily'
-#   end
+SitemapGenerator::Sitemap.default_host = 'https://www.chezvaloupatisserie.fr'
+SitemapGenerator::Sitemap.create do
+  add root_path, :changefreq => 'daily', :priority => 1
 
-# end
-# SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
+  Blogpost.find_each do |post|
+    add blogpost_path(post), :priority => 0.9, :lastmod => post.updated_at, :changefreq => 'daily'
+  end
+
+end
+SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
