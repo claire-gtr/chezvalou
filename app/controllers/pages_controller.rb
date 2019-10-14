9a78e5db8e4c073458cbdf933304ce9595329199
Claire Gautier <claire.gautier@edhec.com>
2019-10-14 14:50:17 +0100

    removed authenticate user for legal page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :legals]
 
   def home
     @blogposts = Blogpost.order('created_at DESC').first(3)
